a8c2d060e2f8daf3d8174cb35da25772823228b3
Leandri Beavis <leandrikuyk@gmail.com>
2022-04-30 18:32:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# JavaScript Fundamentals Quiz
+
+The goal for this project is to build a to timed quiz on JavaScript fundamentals that store high scores. 
+
+## Acceptance Criteria
+
+````
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+````
+
+## How to run this program
+
+1. Open the 'index.html' file in a browser
+OR
+2. View a live preview of my project below
+
+## Live Preview
+
+View a [live preview](https://leandrib.github.io/password-gen/) of my project.
+
+## Screenshot
+
+![Portfolio](./assets/images/taskinator.png)
+
+## Attributions
+
---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+// TODO: Declare Global variables
+
+// TODO: Add timer function
+
+// Quiz questions
+var questions =  [
+    {
+        question: "Commonly used data values do not include ______.",
+        answers: [
+            "boolean",
+            "numbers & strings",
+            "arrays",
+            "alerts",
+        ],
+        correctAnswer: "alerts", // TODO: double check answer
+    },
+    {
+        question: "Arrays in Javascript are used to store ______.",
+        answers: [
+            "booleans",
+            "numbers & strings",
+            "other arrays",
+            "all of the above",
+        ],
+        correctAnswer: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answers: [
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parenthesis",
+        ],
+        correctAnswer: "parenthesis",
+    }, 
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            "JavaScript",
+            "terminal/bash",
+            "for loops",
+            "console.log",
+        ],
+        correctAnswer: "console.log",
+    },
+    {
+        question: "The condition in an if else statement is enclosed with ______.",
+        answers: [
+            "square brackets",
+            "curly brackets",
+            "quotes",
+            "parenthesis",
+        ],
+        correctAnswer: "parenthesis",
+    }
+]
+
+// TODO: Define start quiz functions
+
+    // start timer
+
+// Event Listeners
