c3398db03de58ac9def604c86da87b88d7944b0c
Leandri Beavis <leandrikuyk@gmail.com>
2022-05-01 18:40:17 -0400

    render questions and check answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 // TODO: Declare Global variables
+var question = document.getElementById("question-container");
 var startButton = document.getElementById("start-button");
 var quizIntro = document.getElementById("quiz-intro");
+var quiz = document.getElementById("quiz-container")
+var answerOptions = document.getElementById("answer-options")
+var result = document.getElementById("result");
+var resultText = document.querySelector(".result-txt");
 var seconds = 75;
+
+var currentQuestion = 0;
+// var questionDiv = document.createElement("div");
+
+// var answerDiv= document.createElement("div")
 
 // Quiz questions
 var questions =  [
@@ -13,7 +23,7 @@ var questions =  [
             "arrays",
             "alerts",
         ],
-        correctAnswer: "alerts", // TODO: double check answer
+        correctAnswer: "alerts",
     },
     {
         question: "Arrays in Javascript are used to store ______.",
@@ -57,31 +67,63 @@ var questions =  [
     }
 ]
 
-// TODO: Define start quiz functions
+// Define start quiz functions
 var startQuiz = function() {
     
-    // remove the quiz intro along with the start button on click
+    // Remove quiz intro along with start button click
     quizIntro = quizIntro.remove();
+    question.removeAttribute("hidden");
 
-    startTimer();
+    // Render question
+    showQuestions();
+    // Call startTimer function on button click
+    //startTimer();
 
-    // start timer
-    console.log("start")
 };
 
-// TODO: Add timer function
-var startTimer = function(){
+//  Countdown timer that executes when start button is clicked
+// var startTimer = function(){
 
-    setInterval(function(){
-        if(seconds <= 0){
-        clearInterval(startTimer);
-        document.getElementById("countdown").innerHTML = "Finished";
-        } else {
-            document.getElementById("countdown").innerHTML = "TIME: " + seconds;
-        }
-        seconds -= 1;
-    }, 1000);
+//     setInterval(function(){
+//         if(seconds <= 0){
+//         clearInterval(startTimer);
+//         document.getElementById("countdown").innerHTML = "Sorry, time's up";
+//         } else {
+//             document.getElementById("countdown").innerHTML = "TIME: " + seconds;
+//         }
+//         seconds -= 1;
+//     }, 1000);
+// };
+
+// render questions on page
+var showQuestions = function() {
+
+    var question = questions[currentQuestion];
+    var answers = question.answers;
+
+    var headerEl = document.querySelector(".question-txt");
+    headerEl.textContent = question.question;
+
+    for (i = 0; i < answers.length; i++) {
+        var answer = answers[i];
+        var answerButton = document.querySelector("#answer-button" + i);
+        answerButton.textContent = answer;
+    }
+}
+
+// Check answer against user input
+function checkAnswer(event) {
+    var userInput = event.target;
+    result.style.display = "block";
+
+    if (userInput.textContent === questions[currentQuestion].correctAnswer) {
+        resultText.textContent = "Correct";
+    } else {
+        resultText.textContent = "Incorrect";
+    }
 };
+
 
 // Event Listeners
 startButton.addEventListener("click", startQuiz);
+answerOptions.addEventListener("click", checkAnswer);
