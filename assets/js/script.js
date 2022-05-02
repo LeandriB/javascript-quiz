73926b9fb105f76bbe303ab536e5999d1f71ea48
Leandri Beavis <leandrikuyk@gmail.com>
2022-05-02 00:06:18 -0400

    added functionality to save score and play again

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,13 @@ var resultText = document.querySelector(".result-txt");
 var scoreCard = document.getElementById("score-card");
 var score = document.getElementById("score");
 var leaderboard = document.getElementById("leaderboard-container");
+var submitButton = document.getElementById("submit-button");
+var inputEl = document.getElementById("initials");
+var viewScores = document.querySelector("leaderboard");
+
+// Global Reset variables
+var clearButton = document.getElementById("clear-button");
+var backButton = document.getElementById("back-button");
 
 // Global timer variables
 var time = 75;
@@ -77,7 +84,7 @@ var questions =  [
 var startQuiz = function() {
     
     // Remove quiz intro along with start button click
-    quizIntro = quizIntro.remove();
+    quizIntro.setAttribute("hidden", true);
     question.removeAttribute("hidden");
 
     // Render question
@@ -98,7 +105,7 @@ var showQuestions = function() {
 
     for (i = 0; i < answers.length; i++) {
         var answer = answers[i];
-        var answerButton = document.querySelector("#answer-button" + i);
+        var answerButton = document.getElementById("answer-button" + i);
         answerButton.textContent = answer;
     }
 }
@@ -137,7 +144,7 @@ var nextQuestion = function() {
  //Countdown timer that executes when start button is clicked
 var startTimer = function(){
     timerInterval = setInterval(function() {
-        time--;
+        // time--;
         displayTime();
         if (time < 1) {
             stopQuiz();
@@ -159,7 +166,132 @@ var stopQuiz = function () {
     score.textContent = time;
 }
 
+// Save score on leaderboard
+var saveScore = function(event) {
+    
+    event.preventDefault();
+    
+    if (!inputEl.value) {
+        alert("Enter your initials before submitting");
+        return;
+    }
+
+    var scoreInfo = {
+        initials: inputEl.value,
+        score: time,
+    }
+
+    updateScores(scoreInfo);
+
+    scoreCard.setAttribute("hidden", true);
+    leaderboard.removeAttribute("hidden", true);
+
+    displayScores();
+
+}
+
+// Display highscores
+var displayScores = function() {
+
+    //TODO: Sort scores
+    var sort = sortScores();
+
+    var scoreList = document.getElementById("highscores");
+    scoreList.innerHTML = "";
+    for (i = 0; i < sort.length; i++) {
+        var scoreEntries = sort[i];
+        var scoreEl = document.createElement("li");
+        scoreList.textContent =
+        scoreEntries.initials + " - " + scoreEntries.score;
+        scoreList.append(scoreEl);
+    }
+}
+
+// Sort scores from highest to lowest
+var sortScores = function() {
+    var scoreArray = getScores();
+    if (!scoreArray) {
+        return;
+    }
+
+    scoreArray.sort(function (a, b) {
+        return b.score - a.score;
+    });
+    return scoreArray;
+}
+
+// LocalStorage
+// Updates the leaderboard stored in local storage
+var updateScores = function(scoreInfo) {
+    var scoreArray = getScores();
+    //append new score item
+    scoreArray.push(scoreInfo);
+    localStorage.setItem("scoreArray", JSON.stringify(scoreArray));
+}
+
+//Retrieve scores from local storage and parse object using JSON.parse
+var getScores = function() {
+    var savedScores = localStorage.getItem("scoreArray");
+    if (savedScores !== null) {
+        var scoreArray = JSON.parse(savedScores);
+        return scoreArray;
+    } else {
+        scoreArray = [];
+    }
+    return scoreArray;
+}
+
+// Reset functions
+//clear local storage and display empty leaderboard
+var clearScores = function() {
+    localStorage.clear();
+    // TODO: clear from UI
+    getScores();
+}
+
+//Hide leaderboard card show start card
+var playAgain = function() {
+    leaderboard.setAttribute("hidden", true);
+    quizIntro.removeAttribute("hidden");
+
+    // Reset Timer
+    time = 75;
+    displayTimer.innerHTML = "75"
+    clearInterval(timerInterval);
+
+    // Reset question index
+    currentQuestion = 0;
+
+}
+
+var showHighscores = function() {
+//   TODO: call hide element
+    scoreCard.removeAttribute("hidden");
+
+        // //stop countdown
+        clearInterval(intervalID);
+
+  //assign undefined to time and display that, so that time does not appear on page
+    time = undefined;
+    displayTime();
+
+  // Display scores
+    displayScores();
+}
+
+// Hide Elements
+var HideEl = function() {
+    quiz.setAttribute("hidden", true);
+    questionCard.setAttribute("hidden", true);
+    scoreCard.setAttribute("hidden", true);
+    leaderboardCard.setAttribute("hidden", true);
+}
+
 
 // Event Listeners
 startButton.addEventListener("click", startQuiz);
 answerOptions.addEventListener("click", checkAnswer);
+submitButton.addEventListener("click", saveScore);
+clearButton.addEventListener("click", clearScores);
+backButton.addEventListener("click", playAgain);
+// viewScores.addEventListener("click", showHighscores);
